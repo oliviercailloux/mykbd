2bb320c1cd5627493e60cb3142b0e9b253bbf157
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-04-29 23:13:19 +0200

    Labeled

--- src/main/java/io/github/oliviercailloux/mykbd/App.java
+++ src/main/java/io/github/oliviercailloux/mykbd/App.java
@@ -1,32 +1,48 @@
 package io.github.oliviercailloux.mykbd;
 
-import static com.google.common.base.Verify.verify;
-
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.CharSource;
-import com.google.common.io.Resources;
-import com.google.common.math.DoubleMath;
+import com.google.common.io.MoreFiles;
 import io.github.oliviercailloux.jaris.xml.DomHelper;
 import io.github.oliviercailloux.keyboardd.keyboard.RectangularKeyboard;
 import io.github.oliviercailloux.keyboardd.keyboard.json.JsonRectangularKeyboardReader;
 import io.github.oliviercailloux.keyboardd.keyboard.json.JsonRectangularRowKeyboard;
+import io.github.oliviercailloux.keyboardd.mapping.KeyboardMap;
+import io.github.oliviercailloux.keyboardd.mapping.SimpleSymbolsReader;
 import io.github.oliviercailloux.keyboardd.representable.SvgKeyboard;
+import io.github.oliviercailloux.keyboardd.representable.VisibleKeyboardMap;
 import io.github.oliviercailloux.svgb.PositiveSize;
+import java.io.IOException;
+import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.xml.transform.stream.StreamSource;
+import org.w3c.dom.Document;
 
 public class App {
+  private static final DomHelper DOM_HELPER = DomHelper.domHelper();
+
   public static void main(String[] args) throws Exception {
-    CharSource source = Resources.asCharSource(
-        Path.of("Keyboard layout Elite K70.json").toUri().toURL(), StandardCharsets.UTF_8);
+    rectangular();
+    CharSource source = MoreFiles.asCharSource(
+        Path.of("fr"), StandardCharsets.UTF_8);
+    KeyboardMap map = SimpleSymbolsReader.read(source);
+    VisibleKeyboardMap visible = VisibleKeyboardMap.from(map, ImmutableMap.of());
+    SvgKeyboard svgK = SvgKeyboard.using(DOM_HELPER.asDocument(new StreamSource(new StringReader(Files.readString(Path.of("Elite K70 unlabeled.svg"))))));
+    Document withRepresentations = svgK.withRepresentations(visible::representations);
+    Files.writeString(Path.of("Elite K70 French.svg"), DOM_HELPER.toString(withRepresentations));
+  }
+
+  public static void rectangular() throws IOException {
+    CharSource source = MoreFiles.asCharSource(
+        Path.of("Keyboard layout Elite K70.json"), StandardCharsets.UTF_8);
     JsonRectangularRowKeyboard layout = JsonRectangularKeyboardReader.rowKeyboard(source);
 
     RectangularKeyboard physicalKeyboard = layout.toPhysicalKeyboard(
         PositiveSize.given(1.656d, 1.6d), PositiveSize.given(0.207d, 0.28d));
     SvgKeyboard svgK = SvgKeyboard.zonedFrom(physicalKeyboard);
-    String svg = DomHelper.domHelper().toString(svgK.document());
+    String svg = DOM_HELPER.toString(svgK.document());
     Files.writeString(Path.of("Rectangular Elite K70 unlabeled.svg"), svg);
   }
 }
