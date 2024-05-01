83a5007af73f0a87071e83cb6313dfb60d379d76
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-05-01 02:34:36 +0200

    Started mapping

--- src/main/java/io/github/oliviercailloux/mykbd/Mapping.java
+++ src/main/java/io/github/oliviercailloux/mykbd/Mapping.java
@@ -1,9 +1,16 @@
 package io.github.oliviercailloux.mykbd;
 
+import com.google.common.collect.ImmutableMap;
+import io.github.oliviercailloux.keyboardd.mapping.KeysymEntry;
+import io.github.oliviercailloux.keyboardd.representable.Representation;
+
 public class Mapping {
   public static ImmutableMap<KeysymEntry, Representation> representations() {
     final ImmutableMap.Builder<KeysymEntry, Representation> reprsBuilder =
         new ImmutableMap.Builder<>();
+    reprsBuilder.put(KeysymEntry.mnemonic("Tab"), Representation.fromString("↹"));
+    reprsBuilder.put(KeysymEntry.mnemonic("ISO_Left_Tab"), Representation.fromString("␉"));
+    reprsBuilder.put(KeysymEntry.mnemonic("Return"), Representation.fromString("⏎"));
     reprsBuilder.put(KeysymEntry.mnemonic("comma"), Representation.fromString(","));
     return reprsBuilder.build();
   }
