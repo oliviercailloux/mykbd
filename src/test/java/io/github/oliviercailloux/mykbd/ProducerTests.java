cad2360c00dc2ec7ccfda07fb27e52f3e51bf886
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-10-08 01:30:50 +0200

    mv test French

--- src/test/java/io/github/oliviercailloux/mykbd/ProducerTests.java
+++ src/test/java/io/github/oliviercailloux/mykbd/ProducerTests.java
@@ -9,9 +9,15 @@ import io.github.oliviercailloux.jaris.io.PathUtils;
 import io.github.oliviercailloux.jaris.xml.DomHelper;
 import io.github.oliviercailloux.keyboardd.keyboard.json.JsonRectangularKeyboardReader;
 import io.github.oliviercailloux.keyboardd.keyboard.json.JsonRectangularRowKeyboard;
+import io.github.oliviercailloux.keyboardd.mapping.KeyboardMap;
+import io.github.oliviercailloux.keyboardd.mapping.XkbSymbolsReader;
+import io.github.oliviercailloux.keyboardd.mnemonics.CanonicalKeyboardMap;
+import io.github.oliviercailloux.keyboardd.mnemonics.Mnemonics;
 import io.github.oliviercailloux.keyboardd.representable.RectangularKeyboard;
 import io.github.oliviercailloux.keyboardd.representable.Representation;
 import io.github.oliviercailloux.keyboardd.representable.SvgKeyboard;
+import io.github.oliviercailloux.keyboardd.representable.XKeyNamesAndRepresenter;
+import io.github.oliviercailloux.keyboardd.xkeys.Xkeys;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -36,20 +42,42 @@ public class ProducerTests {
     RectangularKeyboard physicalKeyboard =
         layout.toPhysicalKeyboard(Point.given(1.656d, 1.6d), Point.given(0.207d, 0.28d));
     SvgKeyboard svgK = SvgKeyboard.zonedFrom(physicalKeyboard);
-    String expected = PathUtils.read(PathUtils.fromResource(ProducerTests.class, "Rectangular Elite K70 unlabeled.svg"));
+    String expected = PathUtils
+        .read(PathUtils.fromResource(ProducerTests.class, "Rectangular Elite K70 unlabeled.svg"));
     String result = DOM_HELPER.toString(svgK.document());
     assertEquals(expected, result);
   }
 
   @Test
   void testUnlabeledToX() throws Exception {
-        CloseablePathFactory jsonFactory =
+    CloseablePathFactory jsonFactory =
         PathUtils.fromResource(ProducerTests.class, "Elite K70 unlabeled.svg");
     SvgKeyboard svgK = SvgKeyboard.using(DOM_HELPER.asDocument(jsonFactory.asByteSource()));
-    String expected = PathUtils.read(PathUtils.fromResource(ProducerTests.class, "Elite K70 with X key names.svg"));
+    String expected = PathUtils
+        .read(PathUtils.fromResource(ProducerTests.class, "Elite K70 with X key names.svg"));
     Document out = svgK.withRepresentations(x -> ImmutableList.of(Representation.fromString(x)));
     String result = DOM_HELPER.toString(out);
-    Files.writeString(Path.of("Elite K70 with X key names.svg"), result);
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void testUnlabeledToFrench() throws Exception {
+    CloseablePathFactory jsonFactory =
+        PathUtils.fromResource(ProducerTests.class, "Elite K70 unlabeled.svg");
+    SvgKeyboard inputSvg = SvgKeyboard.using(DOM_HELPER.asDocument(jsonFactory.asByteSource()));
+    inputSvg.setFontSize(9d);
+    
+    KeyboardMap map = XkbSymbolsReader.common().overwrite(XkbSymbolsReader.fr());
+    CanonicalKeyboardMap canonMap = CanonicalKeyboardMap
+        .canonicalize(map.canonicalize(Xkeys.latest().canonicalByAlias()), Mnemonics.latest());
+    XKeyNamesAndRepresenter representer =
+        XKeyNamesAndRepresenter.from(canonMap, XKeyNamesAndRepresenter::defaultRepresentation);
+    Document out = inputSvg.withRepresentations(representer::representations);
+
+    String expected = PathUtils
+        .read(PathUtils.fromResource(ProducerTests.class, "Elite K70 French.svg"));
+    String result = DOM_HELPER.toString(out);
+    Files.writeString(Path.of("Elite K70 French.svg"), result);
     assertEquals(expected, result);
   }
 }
